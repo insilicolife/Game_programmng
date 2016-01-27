4c19774301e2a36be4e21d8255888b9746eb47bb
Nigatu <babyawet@gmail.com>
2016-01-27 17:35:08 +0200

    initial commit

---
+++ Chess_Game_in_c/main.c
@@ -0,0 +1,549 @@
+
+//----------------------------------------Nigatu Adossa------------------------------------------------------------------
+//----------------------------------------TI0905219----------------------------------------------------------------------
+//----------------------------------------21/01/2011---------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define BORDSIZE 64
+#define INVALID_RANK 10
+#define INVALID_FILE 9
+#define KILLED 100
+#define HORIZONTAL_LENGTH 8
+#define RIGHT_MOVE 2
+#define WRONG_MOVE 1
+#define WHITE 0
+#define BLACK 1
+#define UNDEFINED -1
+#define STOP 0
+#define KEYVORD_INPUT 10
+
+//--------------------------global variable declaration-----------------------------------------------------------------
+typedef struct chess_pieces// data type to store chess piece information
+{
+    int rank;
+    char file;
+    char piece_name;
+    char color;
+}
+chess_piece;
+
+chess_piece setup[BORDSIZE];//a structure that holde the ongoing movments of the peices
+
+chess_piece bourder[BORDSIZE]= //initial peice setup information
+{
+    {
+        1,'a','r','w'
+    },
+    {
+        1,'b','n','w'
+    },
+    {
+        1,'c','b','w'
+    },
+    {
+        1,'d','q','w'
+    },
+    {
+        1,'e','k','w'
+    },
+    {
+        1,'f','b','w'
+    },
+    {
+        1,'g','n','w'
+    },
+    {
+        1,'h','r','w'
+    },
+
+    {
+        2,'a','p','w'
+    },
+    {
+        2,'b','p','w'
+    },
+    {
+        2,'c','p','w'
+    },
+    {
+        2,'d','p','w'
+    },
+    {
+        2,'e','p','w'
+    },
+    {
+        2,'f','p','w'
+    },
+    {
+        2,'g','p','w'
+    },
+    {
+        2,'h','p','w'
+    },
+
+    {
+        3,'a',' ',' '
+    },
+    {
+        3,'b',' ',' '
+    },
+    {
+        3,'c',' ',' '
+    },
+    {
+        3,'d',' ',' '
+    },
+    {
+        3,'e',' ',' '
+    },
+    {
+        3,'f',' ',' '
+    },
+    {
+        3,'g',' ',' '
+    },
+    {
+        3,'h',' ',' '
+    },
+
+    {
+        4,'a',' ',' '
+    },
+    {
+        4,'b',' ',' '
+    },
+    {
+        4,'c',' ',' '
+    },
+    {
+        4,'d',' ',' '
+    },
+    {
+        4,'e',' ',' '
+    },
+    {
+        4,'f',' ',' '
+    },
+    {
+        4,'g',' ',' '
+    },
+    {
+        4,'h',' ',' '
+    },
+
+    {
+        5,'a',' ',' '
+    },
+    {
+        5,'b',' ',' '
+    },
+    {
+        5,'c',' ',' '
+    },
+    {
+        5,'d',' ',' '
+    },
+    {
+        5,'e',' ',' '
+    },
+    {
+        5,'f',' ',' '
+    },
+    {
+        5,'g',' ',' '
+    },
+    {
+        5,'h',' ',' '
+    },
+
+    {
+        6,'a',' ',' '
+    },
+    {
+        6,'b',' ',' '
+    },
+    {
+        6,'c',' ',' '
+    },
+    {
+        6,'d',' ',' '
+    },
+    {
+        6,'e',' ',' '
+    },
+    {
+        6,'f',' ',' '
+    },
+    {
+        6,'g',' ',' '
+    },
+    {
+        6,'h',' ',' '
+    },
+
+    {
+        7,'a','p','b'
+    },
+    {
+        7,'b','p','b'
+    },
+    {
+        7,'c','p','b'
+    },
+    {
+        7,'d','p','b'
+    },
+    {
+        7,'e','p','b'
+    },
+    {
+        7,'f','p','b'
+    },
+    {
+        7,'g','p','b'
+    },
+    {
+        7,'h','p','b'
+    },
+
+    {
+        8,'a','r','b'
+    },
+    {
+        8,'b','n','b'
+    },
+    {
+        8,'c','b','b'
+    },
+    {
+        8,'d','q','b'
+    },
+    {
+        8,'e','k','b'
+    },
+    {
+        8,'f','b','b'
+    },
+    {
+        8,'g','n','b'
+    },
+    {
+        8,'h','r','b'
+    },
+};
+
+//---------------------------------------game logic functions--------------------------------------------------------------
+
+//--------------------------------finding rank cordinate in the chass board-------------------------------------------
+int rankf_axis(int c)
+{
+    switch(c)
+
+    {
+    case 0:
+        return 8;
+        break;
+    case 1:
+        return 7;
+        break;
+    case 2:
+        return 6;
+        break;
+    case 3:
+        return 5;
+        break;
+    case 4:
+        return 4;
+        break;
+    case 5:
+        return 3;
+        break;
+    case 6:
+        return 2;
+        break;
+    case 7:
+        return 1;
+        break;
+    case 8:
+        return 0;
+        break;
+    default:
+        return INVALID_RANK;
+        break;
+    }
+}
+
+
+//-------------------------------------printing the status of chass board to the screen----------------------------------
+void print_setup()
+{
+    int x,y;
+    char files='a';
+
+    for(y=0; y<HORIZONTAL_LENGTH; y++)
+    {
+
+        printf("\n\n\n\n            %d",rankf_axis(y));
+
+        for(x=0; x<HORIZONTAL_LENGTH; x++)
+        {
+            printf(" %3c",setup[HORIZONTAL_LENGTH*y+x].color);
+            printf("%c ",setup[HORIZONTAL_LENGTH*y+x].piece_name);
+
+
+        }
+    }
+    printf("\n\n            %c",' ');
+    for(y=0; y<HORIZONTAL_LENGTH; y++)
+    {
+        printf("%4c  ",files);
+        files++;
+    }
+
+}
+//------------------------------------printing the initial chess board-----------------------------------------------
+void draw_setup()
+{
+    int i;
+    for(i=0; i<BORDSIZE; i++)
+    {
+
+        setup[i]=bourder[i];
+
+    }
+    print_setup();
+}
+
+//----------------------------------finding the file coortdinate in the chass board------------------------------------
+int filecodnat(char z)
+{
+    switch(z)
+    {
+    case 'a':
+        return 0;
+        break;
+    case 'b':
+        return 1;
+        break;
+    case 'c':
+        return 2;
+        break;
+    case 'd':
+        return 3;
+        break;
+    case 'e':
+        return 4;
+        break;
+    case 'f':
+        return 5;
+        break;
+    case 'g':
+        return 6;
+        break;
+    case 'h':
+        return 7;
+        break;
+    default:
+        return INVALID_FILE;
+        break;
+    }
+}
+//-----------------------------function that saves the game state to the disc--------------------------------------
+int save_game_status()
+{
+
+    FILE* save_to_disc;
+    save_to_disc=fopen("write_to_file.dat","wb");//open write_to_file.dat file to write game state
+    if(save_to_disc==NULL)
+    {
+        printf("error!! \n");//return error massage if the file does not exist
+        return 1;
+    }
+    else
+
+        fwrite(setup,1,sizeof(setup),save_to_disc);//write array of structure to the disck
+
+    fclose(save_to_disc);//close the file
+
+    return 0;
+}
+
+// ------------------------------------moving chass peices in the board----------------------------------------------
+
+int move(char filefrom,int rankfrom,char fileto,int rankto)
+{
+
+    if((rankfrom==INVALID_RANK)||((filecodnat(filefrom))==INVALID_FILE))// if original piece is not in the chass board cordinate
+    {
+        printf("the peice you want to move does not exist! \n");
+        return WRONG_MOVE;
+
+    }
+    else if(((rankto)==INVALID_RANK)||((filecodnat(fileto))==INVALID_FILE))//the destination coordenate is not in the chass board
+    {
+        printf("moving to out of bourder! \n");
+
+        return WRONG_MOVE;
+
+    }
+
+    //else if(setup[filecodnat(filefrom)][rankfrom]==" ") //tring to move empity pieces
+    else if(setup[filecodnat(filefrom)+rankfrom*HORIZONTAL_LENGTH].piece_name==' ') //tring to move empity pieces
+    {
+        printf("there is no peices in original position\n");
+
+        return WRONG_MOVE;
+    }
+    else if(setup[filecodnat(filefrom)+rankfrom*HORIZONTAL_LENGTH].rank==KILLED) //Trying to move killed peice
+    {
+        //In the case of this program there would not be a case that this will apear
+        printf("This peice is moved out of the game\n");
+        return WRONG_MOVE;
+    }
+
+    else
+    {
+
+        printf("right move! \n");
+
+        if(setup[filecodnat(fileto)+rankto*HORIZONTAL_LENGTH].piece_name==' ')
+        {
+            setup[filecodnat(fileto)+rankto*HORIZONTAL_LENGTH]=setup[filecodnat(filefrom)+rankfrom*HORIZONTAL_LENGTH];
+        }
+        else
+        {
+
+            printf("%c%c is killed",setup[filecodnat(fileto)+rankto*HORIZONTAL_LENGTH].color,setup[filecodnat(fileto)+rankto*HORIZONTAL_LENGTH].piece_name);//eats the existing peices
+
+            setup[filecodnat(fileto)+rankto*HORIZONTAL_LENGTH]=setup[filecodnat(filefrom)+rankfrom*HORIZONTAL_LENGTH];
+        }
+        //The peices is moved out of the game
+        setup[filecodnat(filefrom)+rankfrom*HORIZONTAL_LENGTH].piece_name=' ';
+        setup[filecodnat(filefrom)+rankfrom*HORIZONTAL_LENGTH].file='z';
+        setup[filecodnat(filefrom)+rankfrom*HORIZONTAL_LENGTH].rank=100;
+        setup[filecodnat(filefrom)+rankfrom*HORIZONTAL_LENGTH].color=' ';
+
+
+        print_setup();//prints the game statuse to the computer screen
+
+    }
+    save_game_status();//saves Game State when there is a move
+
+    return RIGHT_MOVE;
+}
+
+//---------------------------function to read game state from disk--------------------------------------------------
+void read_game_state_from_disk()
+{
+    FILE *disk;
+    int i;
+    int count;
+    chess_piece setup_disk[BORDSIZE];
+
+    disk=fopen("write_to_file.dat","rb");//open a file to read a game state
+
+    if(disk!=NULL)
+
+        count=fread(setup_disk, sizeof(chess_piece),BORDSIZE, disk);//
+
+    if(count==BORDSIZE)//if the structure read from the disk is the size of the BORDSIZE, then file reading is successful
+    {
+
+        for(i=0; i<BORDSIZE; i++)
+        {
+            setup[i]=setup_disk[i];//copy structure from disck to global struct variable one by one
+        }
+
+        fclose(disk);//close the file
+        print_setup();//print the game status to the computer screen
+    }
+}
+//------------------------------checks the color of the peice-------------------------------------------------------
+int give_turnf(char x)
+{
+
+    if(x=='w')
+        return BLACK;
+    else if(x=='b')
+        return WHITE;
+    else
+        return UNDEFINED;
+}
+//--------------------------------Main function-------------------------------------------------------------------
+int main(void)
+{
+
+    int i=2;
+    int a;
+    char str[KEYVORD_INPUT];
+    int give_turn;
+    int check;
+    char z;
+    printf("                   Nigatu Adossa\n                   TI0905219\n");
+    printf("                   Information Technology\n");
+    printf("                   programming a chess Game in c\n\n ");
+
+
+
+    int  originalrank,destinationrank;
+    char originfile,destinationfile;
+
+    printf("\n 'n' to start new game \n 'p' to resume previous game \n");
+    scanf("%c",&z);
+
+    if(z=='p')//reading game status from the disk
+    {
+        printf(" Here is previous game status");
+        read_game_state_from_disk();
+    }
+    else
+    {
+
+
+        printf("\n new gmae\n  game rule:it will only checks weather the peice movment is valid\n           chess game rules  will be applied in the next phase of this project.\n  \n");
+        printf(" key:  \n bp black pawn  \n bk black king  \n bn black knight  \n bb black bishop  \n br black rook  \n bq black queen  \n wp white pawn  \n wk white king  \n wn white knight  \n wb white bishop  \n wr white rook  \n wq white queen  \n" );
+        draw_setup();//draw initial game status
+    }
+
+    printf("\n\n");
+
+    while(a)
+    {
+        do
+        {
+            printf("\ngive coordinates in the form of a1c4, press any key if you want to save & stop!\n al- a-original file peice coordinate 1-original rank peice coordinate\n c4- c-destination file coordinate 4-destination rank coordinate\n");
+
+            do
+            {
+                give_turn=(i%2);
+                if(give_turn==BLACK)//to keep giving a chance to white and black peices
+                    printf("\n now it is black's piece's turn!\n");
+                else
+                    printf("\n now it is white's piece's turn!\n");
+
+
+                scanf("%s",&str);
+
+                check=sscanf(str,"%c%d%c%d",&originfile,&originalrank,&destinationfile,&destinationrank);
+
+                if(check==1 || (int)str[0]==STOP)//if the user wants to quit
+                {
+                    save_game_status();//save the game status to the disck
+                    return STOP;
+                }
+
+            }
+            while(give_turn==give_turnf(setup[filecodnat(originfile)+rankf_axis(originalrank)*HORIZONTAL_LENGTH].color)); //checks if the player is playing in his/her turn
+
+            i++;//increment the value of i to give turns to other player
+        }
+        while( move(originfile,rankf_axis(originalrank),destinationfile,rankf_axis(destinationrank))!=RIGHT_MOVE);//give chance to player until it plays correctlly
+
+    }
+
+    return 0;
+}
+//-----------------------------------------------CopyRight Nigatu Adossa-----------------------------------------------------------
+
---
+++ snack_game_cpp/main.cpp
@@ -0,0 +1,502 @@
+//----------------------------------------By: Nigatu Adossa------------------------------------------------------------------
+//----------------------------------------Chess game programming----------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------
+
+#include <cstdlib>
+#include <iostream>
+#include <conio.h>
+#include <fstream>
+//#include <graphics.h>
+#include <time.h>
+//#include "SDL.h"
+//#include <system.h>
+#include <windows.h>
+
+
+using namespace std;
+
+void gotoxy(int x,int y)
+{
+    HANDLE hConsole;
+    COORD cursorLoc;
+    std::cout.flush();
+    cursorLoc.X = x;
+    cursorLoc.Y = y;
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hConsole, cursorLoc);
+}
+
+
+
+class draw
+{
+private:
+
+    int raw;
+    int col;
+    /*int score;
+    int High_score;
+    int speed;*/
+
+public:
+    draw(int c=75,int d=20);
+    void draw_border();
+    //void gotoxy(int x, int y);
+    int randomvalue (int starting, int ending); // Return a random int value between end and starting parameters
+    void put_food();
+
+
+};
+class snake: public draw
+{
+    //private:
+
+
+
+public:
+
+    int length;
+    int head_x;
+    int head_y;
+    int tail_x;
+    int tail_y;
+    int tail_dir;
+    int head_dir;
+    int bend_x[1];
+    int bend_y[1];
+    int prev;
+    int now;
+
+    char snk[75][20];
+    snake(int length, int head_x, int head_y, int tail_x, int tail_y, int head_dir, int tail_dir,int bend_x0,int bend_x1, int bend_y0,int bend_y1 );
+    void make_snake();
+    //void move_snake();
+};
+/*class play_game:public snake
+{
+    private:
+    int score;
+    int highscore;
+    int start;
+    int paouse;
+    int speed;
+    public:
+    play_game(int score0,int highcore0,int start0,int paouse0);
+    int give_score();
+    int give_highscore;
+    bool colision_ditacter();
+};*/
+
+
+draw::draw(int c,int d)
+{
+    raw=c;
+    col=d;
+}
+void draw::draw_border()
+{
+    gotoxy(0,0);
+    for (int j=0;j<raw;j++)
+    {
+        cout<<"x";
+    }
+    //gotoxy(1,col);
+    for (int i=0;i<col;i++)
+        cout<<"x"<<endl;
+    //gotoxy(0,1);
+    for (int k=0;k<raw;k++)
+        cout<<"x";
+
+    for (int z=0;z<col;z++)
+    {
+        gotoxy(raw,z+1);
+        cout<<"x";
+    }
+    gotoxy(0,col+2);
+    for (int a;a<80;a++)
+    {
+        cout<<'-';
+
+    }
+    cout<<endl<<"score=     "<<"High score=         "<<"speed=      "<<endl;
+}
+int draw::randomvalue(int starting, int ending) // Return a random int value between end and starting parameters
+
+{
+    static int previous = 0;
+    int answer;
+    if (previous <0) previous = 0;
+    int difference = starting - ending;
+    time_t t;
+    srand((unsigned) time(&t));
+    for (int i = 0; i <previous;i++) rand ();
+    answer = (rand () % difference);
+    previous++;
+    return answer;
+}
+/*void draw::put_food()
+{
+static int foodcount = 0;//Keep Count of food
+int futurex, futurey, futurepixel;
+int i;
+char scorestring [100];
+if (foodcount < 500) //Adds a food if no food is present and upto maximum 500 food
+{
+int valid = 0;
+int foodx;
+int foody;
+while (!valid)
+{
+foodx = randomvalue (15,1003);
+foody = randomvalue (15,695);
+if (getpixel (foodx,foody)!= 15)
+{
+putpixel (foodx,foody,2);
+foodcount++;
+valid = 1;
+}
+}
+ }
+}*/
+
+void draw::put_food()
+{
+    for (int i=0;i<5;i++)
+    {
+        gotoxy(randomvalue(0,50),randomvalue(0,20));
+        cout<<"�";
+    }
+}
+
+snake::snake(int length0, int head_x0, int head_y0, int tail_x0, int tail_y0,int head_dir0,int tail_dir0,int bend_x0, int bend_x1,int bend_y0,int bend_y1)
+{
+    length=length0;
+    head_x=head_x0;
+    head_y=head_y0;
+    tail_x=tail_x0;
+    tail_y=tail_y0;
+    head_dir=head_dir0;
+    tail_dir=tail_dir0;
+    bend_x[0]=bend_x0;
+    bend_x[1]=bend_x1;
+    bend_y[0]=bend_y0;
+    bend_y[1]=bend_y1;
+
+
+
+}
+void snake::make_snake()
+{
+
+    for (int  i=0;i<length;i++)
+    {
+        gotoxy(head_x,head_y);
+        snk[head_x][head_y]='~';
+        cout<<snk[head_x][head_y];
+
+        head_x++;
+
+        //Sleep(1000);
+
+    }
+}
+
+/*void snake::move_snake(snake a,int keypress)
+    {   //head_dir=keypress;
+        if(head_dir==6)
+        head_x++;
+        make_snake();
+        Sleep(1000);
+        if(head_dir==4)
+        tail_x--;
+        make_snake();
+        Sleep(1000);
+
+        if(head_dir==8)
+        head_y++;
+        make_snake();
+        Sleep(1000);
+        if(head_dir==2)
+        head_dir--;
+        make_snake();
+        Sleep(1000);
+
+    }*/
+
+/*char snake0[5];
+for(int  i=0;i<4;i++)
+{
+         gotoxy(35,10);
+         int j=0;
+         snake0[j]='~';
+         cout<<snake0[j];
+         j++;
+}*/
+
+void move(snake a)
+{
+    a.make_snake();
+    a.tail_x=a.head_x-(a.length);
+    a.tail_y=a.head_y;
+//    putpixel(12,25,6);
+    //int keypress;
+
+    //if(kbhit());
+    //a.head_dir=cin.get();
+    for (int i=0;i<50;i++)
+    {
+        a.now=a.head_dir;
+    if(kbhit())
+    cin>>a.head_dir;
+    a.prev=a.now;
+    a.now=a.head_dir;
+   //if(kbhit())
+
+   //a.head_dir=getche();
+
+
+         do{
+
+           /* if(a.prev==2)
+            {
+            a.tail_x=a.head_x;
+            a.tail_y=a.head_y-a.length;
+            }
+            else if(a.prev==8)
+            {
+            a.tail_x=a.head_x;
+            a.tail_y=a.head_y+a.length;
+            }
+            else
+            {
+            a.tail_x=a.head_x-(a.length);
+            a.tail_y=a.head_y;
+            }*/
+
+
+
+            ++a.head_x;
+            a.snk[a.head_x][a.head_y]='~';
+            gotoxy(a.head_x-1,a.head_y);
+            cout<<a.snk[a.head_x][a.head_y];
+            Sleep(100);
+
+            gotoxy(a.tail_x,a.tail_y);
+            a.snk[a.tail_x][a.tail_y]=' ';
+            cout<<a.snk[a.tail_x][a.tail_y];
+            //a.tail_x++;
+
+            if(/*(a.prev==2) && (*/a.tail_y<a.head_y)
+            {
+                         a.tail_y++;}
+            else if(a.tail_y>a.head_y)
+            a.tail_y--;
+
+            else{
+
+                         a.tail_x++;}
+
+
+          // if(/*(a.prev==8)&& (*/a.tail_y>a.head_y){
+                        //   a.tail_y--;}
+           // else {
+                          // a.tail_x++;}
+
+            Sleep(100);
+
+            if(kbhit())
+
+            cin>>a.head_dir;
+            a.prev=a.now;
+            a.now=a.head_dir;
+
+            }while(a.head_dir==6);
+
+
+
+
+        /*{
+            //for(a.tail_x;a.tail_x<a.head_x;a.tail_x++)
+            //{
+
+            //int x=a.tail_x;
+                   for(a.tail_x;a.tail_x<50;)
+                    {
+
+                int x=a.tail_x;
+                a.snk[a.tail_x][a.tail_y]=' ';
+                a.head_x++;
+                a.tail_x++;
+
+                a.snk[a.head_x][a.head_y]='~';
+                //for(x;x<=a.head_x;x++)
+                //cout<<a.snk[x][a.tail_y];
+
+                 cout<<a.snk[x+1][a.tail_y];
+                 Sleep(1000);
+
+                  cout<<a.snk[x+2][a.tail_y];
+                  Sleep(1000);
+                   cout<<a.snk[x+3][a.tail_y];
+                   Sleep(1000);
+                 //cout<<a.snk[4][a.tail_y];
+                cout<<a.snk[a.head_x][a.tail_y];
+                //Sleep(1000);
+                Sleep(1000);
+
+                        /*for(a.tail_x-1;<a.tail_x(a.tail_x+5);a.tail_x++)
+                        {
+                            a.snk[a.tail_x]=' ';
+                            a.tail_x++;
+                            a.head_x++;
+                            a.snk[a.head_x]='~';
+                        cout<<a.snk[x];
+                        Sleep(1000);
+                        }
+                        Sleep(1000);*/
+        /*cout<<a.snk[a.tail_x+1];
+        Sleep(1000);
+        cout<<a.snk[a.tail_x+2];
+        Sleep(1000);
+        cout<<a.snk[a.tail_x+3];
+        Sleep(1000);
+        cout<<a.snk[a.tail_x+4];
+        Sleep(1000);
+        }*/
+        //Sleep(1000);
+        //}
+
+
+        //if (a.head_dir==4)
+        do{
+
+            //a.head_x=a.tail_x;
+            //a.tail_x=a.head_x;
+            //a.head_x--;
+            /*if(a.prev==2)
+            {
+            a.tail_x=a.head_x;
+            a.tail_y=a.head_y-a.length;}
+            if(a.prev==8)
+            {
+            a.tail_x=a.head_x;
+            a.tail_y=a.head_y+a.length;
+            }
+
+            a.tail_x=a.head_x-(a.length);
+            a.tail_y=a.tail_y;*/
+            a.head_x--;
+
+            a.snk[a.head_x][a.head_y]='~';
+            gotoxy(a.head_x,a.head_y);
+            cout<<a.snk[a.head_x][a.head_y];
+            Sleep(100);
+
+            a.snk[a.tail_x][a.tail_y]=' ';
+            gotoxy(a.tail_x,a.tail_y);
+            cout<<a.snk[a.tail_x][a.tail_y];
+            if(a.tail_y>a.head_y)
+            a.tail_y--;
+            else if(a.tail_y<a.head_y)
+            a.tail_y++;
+            else
+            a.tail_x--;
+
+            Sleep(100);
+            if(kbhit()){
+
+            cin>>a.head_dir;
+            a.prev=a.now;
+            a.now=a.head_dir;}
+
+        }while(a.head_dir==4);
+
+
+        //if (a.head_dir==2)
+        do{
+
+            ++a.head_y;
+            a.snk[a.head_x][a.head_y]='~';
+            gotoxy(a.head_x,a.head_y-1);
+            cout<<a.snk[a.head_x][a.head_y];
+            Sleep(100);
+
+            gotoxy(a.tail_x,a.tail_y);
+            a.snk[a.tail_x][a.tail_y]=' ';
+            cout<<a.snk[a.tail_x][a.tail_y];
+            if (a.tail_x<a.head_x)
+                ++a.tail_x;
+                //++a.tail_y;}
+
+            else
+                ++a.tail_y;
+
+            Sleep(100);
+
+            if(kbhit()){
+
+            cin>>a.head_dir;
+            a.prev=a.now;
+            a.now=a.head_dir;}
+        }while(a.head_dir==2);
+        //if (a.head_dir==8)
+        do{
+            --a.head_y;
+            a.snk[a.head_x][a.head_y]='~';
+            gotoxy(a.head_x,a.head_y);
+            cout<<a.snk[a.head_x][a.head_y];
+            Sleep(100);
+            gotoxy(a.tail_x,a.tail_y);
+            a.snk[a.tail_x][a.tail_y]=' ';
+            cout<<a.snk[a.tail_x][a.tail_y];
+            if (a.tail_x<a.head_x)
+                ++a.tail_x;
+            else
+                --a.tail_y;
+            Sleep(100);
+        if(kbhit()){
+
+            cin>>a.head_dir;
+            a.prev=a.now;
+            a.now=a.head_dir;}
+        }while(a.head_dir==8);
+        do
+        {
+        break;
+        }while(a.head_dir==5);
+        /*if(keypress==8)
+            {
+
+
+
+            }*/
+
+        //}
+
+
+
+    }
+}
+
+int main()
+{
+
+    //draw x;
+
+    //x.put_food();
+
+    //x.draw_border();
+    snake y(4,15,5,11,5,6,6,0,0,0,0);
+    y.draw_border();
+    //y.make_snake();
+
+    y.put_food();
+     move(y);
+
+    y.draw_border();
+    //sleep(1000);
+    system("PAUSE");
+    return 0;
+}
+
+//-----------------------------------------------CopyRight Nigatu Adossa-----------------------------------------------------------
